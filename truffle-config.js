e9f57e367a0b0d58effc411f34d5032e0c4f530e
M-Killer-dev <mykhailokoroldev@outlook.com>
2024-10-20 16:49:43 -0700

    add the network for deployment the smart contract

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,9 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const SECRET_KEY = process.env.SECRET_KEY;
+const infuraProjectId = process.env.INFURA_PROJECT_ID;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -6,6 +12,22 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
-    }
-  }
+    },
+    sepolia: {
+      provider: () => new HDWalletProvider(SECRET_KEY, `https://mainnet.infura.io/v3/${infuraProjectId}`),
+      network_id: 11155111, // Sepolia's network ID
+      gas: 5500000, // Gas limit
+      confirmations: 10, // Confirmations to wait between deployments
+      timeoutBlocks: 200, // Blocks before deployment times out
+      skipDryRun: true, // Skip dry run before migrations
+      pollingInterval: 8000,
+      networkCheckTimeoutnetworkCheckTimeout: 10000,
+      gasLimit: 7500000,
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.8.0",  // Use specific solc version
+    },
+  },
 }
